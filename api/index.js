31eb76a0df0fe0d14384ae567746da832665aeab
Gianfranco Elli <gianelli99@hotmail.com>
2021-10-05 19:28:32 -0300

    basic server created

--- api/index.js
+++ api/index.js
@@ -1,1 +1,12 @@
-console.log("Working");
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const port = process.env.PORT;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, async () => {
+  console.log(`Server running on ${port}`);
+});
