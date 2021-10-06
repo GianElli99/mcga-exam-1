cb436b67348ef5508a581abde1f5dc74f84fd4de
Gianfranco Elli <gianelli99@hotmail.com>
2021-10-06 17:09:19 -0300

    add and modify controlled added

--- api/controllers/products.controller.js
+++ api/controllers/products.controller.js
@@ -1,30 +1,64 @@
 const Product = require('../models/product.model');
+const { request, response } = require('express');
 
-const getProducts = async (req, res) => {
+const getProducts = async (req = request, res = response) => {
   try {
     res.json('GET PRODUCTS');
   } catch (error) {}
 };
 
-const getProductById = async (req, res) => {
+const getProductById = async (req = request, res = response) => {
   try {
     res.json('GET PRODUCT BY ID');
   } catch (error) {}
 };
 
-const addProduct = async (req, res) => {
+const addProduct = async (req = request, res = response) => {
   try {
-    res.json('ADD PRODUCT');
-  } catch (error) {}
+    const product = new Product(req.body);
+
+    const nameAlreadyUsed = await Product.findOne({ name: product.name });
+    if (nameAlreadyUsed) {
+      res.status(400).json({
+        error: 'A product with the same name already exists.',
+      });
+    } else {
+      await product.save();
+      res.status(201).json(product);
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'An internal server error ocurred.' });
+  }
 };
 
-const modifyProduct = async (req, res) => {
+const modifyProduct = async (req = request, res = response) => {
   try {
-    res.json('MODIFY PRODUCT');
-  } catch (error) {}
+    const { productId } = req.params;
+
+    const nameAlreadyUsed = Product.findOne({
+      name: req.body.name,
+      _id: { $ne: productId },
+    });
+    if (nameAlreadyUsed) {
+      return res.status(400).json({
+        error: 'A product with the same name already exists.',
+      });
+    }
+
+    const product = await Product.findByIdAndUpdate(productId, req.body, {
+      new: true,
+    });
+    if (product) {
+      res.json(tecnico);
+    } else {
+      res.status(404).json({ error: 'Resource not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'An internal server error ocurred.' });
+  }
 };
 
-const deleteProduct = async (req, res) => {
+const deleteProduct = async (req = request, res = response) => {
   try {
     res.json('DELETE PRODUCT');
   } catch (error) {}
