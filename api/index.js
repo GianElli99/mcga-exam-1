650ac93da609a6325ae9e8eeb3734fa7db86987d
Gianfranco Elli <gianelli99@hotmail.com>
2021-10-06 12:07:43 -0300

    connection to mongodb added

--- api/index.js
+++ api/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const { connectDB } = require('./database/connectDB');
+
 const port = process.env.PORT;
+const connectionStringDB = process.env.MONGO_CONNECTION;
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.listen(port, async () => {
   console.log(`Server running on ${port}`);
+  await connectDB(connectionStringDB);
 });
